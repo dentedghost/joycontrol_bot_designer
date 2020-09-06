05935d551f2bab75afb40c7be9248ed4eb8de605
Steven Miller <s_c_m_jr@yahoo.com>
2020-09-05 22:43:23 -0700

    jbd_version_0.1.2 : Minor changes

--- web.py
+++ web.py
@@ -210,6 +210,11 @@ def stop():
 
 
 def files_scanner(path):
+    # search_dir = "/mydir/"
+    # os.chdir(search_dir)
+    # files = filter(os.path.isfile, os.listdir(search_dir))
+    # files = [os.path.join(search_dir, f) for f in files]  # add path to each file
+    # files.sort(key=lambda x: os.path.getmtime(x))
     for file in os.listdir(path):
         if os.path.isfile(os.path.join(path, file)):
             yield file
